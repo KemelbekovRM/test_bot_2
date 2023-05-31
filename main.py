56092bc6f5a5b2a6e5ccc254c717654e3fcdd462
Rustam <Rustam.kemelbekov@gmail.com>
2023-06-01 00:09:41 +0300

    second commit

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # написать на айограм эхо бота который будет получать два числа,
 # спрашивать что с ними делать через меню и присылать ответ
 
-import aiogram
+#import aiogram
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-print('Поставил ГИТ и втух, только не надо ныть ')
+bot = Bot (token='5969349861:AAFYdb1VjC5wnP7TBYKEhGkvL-4Q37ZN8WQ')
+dp = Dispatcher(bot)
+
+@dp.message_handlers()
+async def echo(message: types.Message):
+    await message.answer(message.text)
+
+executor.start_polling(dp, skip_updates=True)
+#print('Поставил ГИТ и втух, только не надо ныть ')
